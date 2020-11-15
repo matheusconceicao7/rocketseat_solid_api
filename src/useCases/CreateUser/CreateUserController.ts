5424a1d01c67ad31c47ab5ea099ad7fba180fc0c
Matheus <matheuscosta2006@gmail.com>
2020-11-15 16:53:15 -0300

    Create solid api

---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,20 @@
+import { CreateUserUseCase } from './CreateUserUseCase';
+import { Request, Response } from 'express';
+
+export class CreateUserController {
+    constructor(
+        private CreateUserUseCase: CreateUserUseCase
+    ) { };
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, email, password } = request.body;
+        try {
+            await this.CreateUserUseCase.execute({ name, email, password })
+            return response.status(201).send()
+        } catch (error) {
+            response.status(400).json({
+                message: error.message || 'Unexpected error.'
+            })
+        }
+    }
+}
